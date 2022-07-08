a85550f99a09e2173e74d5e26be8961c54bf030c
admin <admin@salontime.in>
2022-07-08 18:30:20 +0530

    Add dropdown navbar item

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,15 @@ function Header(){
         </h1>
         <ul className='nav-items'>
           <li>Home</li>
-          <li>Contests</li>
+          <li class="dropdown">
+            <span>Contests</span>
+            <i class="material-icons">arrow_drop_down</i>
+            <div class="dropdown-content">
+              <span>Link 1</span>
+              <span>Link 2</span>
+              <span>Link 3</span>
+            </div>
+          </li>
           <li>About</li>
         </ul>
         {/* <img src="" className="App-logo" alt="logo" /> */}
