3da4600a1ddc7ff49e9d277c70ea1d83eec9db59
admin <admin@salontime.in>
2022-08-20 20:26:21 +0530

    Add pre-caching and dynamic caching in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,28 @@
+var STATIC_CACHE = "static";
+var DYNAMIC_CACHE = "dynamic";
+
 self.addEventListener("install", event => {
-    console.log("Service Worker is being installed!", event);
-    event.waitUntil(caches.open("static")
-      .then(cache => {
-        console.log("[Service Worker] Pre-caching App Shell!");
-        cache.add("index.html");
-        cache.add("main.js");
-      }));
+  console.log("Service Worker is being installed!", event);
+  event.waitUntil(caches.open(STATIC_CACHE)
+    .then(cache => {
+      console.log("[Service Worker] Pre-caching App Shell!");
+      cache.addAll([
+        "/index.html",
+        "/main.js"
+      ]);
+    }));
 });
 
 self.addEventListener("activate", event => {
     console.log("Service Worker getting activated!", event);
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if(key !== STATIC_CACHE && key !== DYNAMIC_CACHE){
+          console.log("[Service Worker] Removing old cache", key);
+          return caches.delete(key);
+        }
+      }));
+    })
     return self.clients.claim();
 });
 
@@ -20,7 +33,14 @@ self.addEventListener('fetch', event => {
           if(response){
             return response;
           } else {
-            return fetch(event.request);
+            return fetch(event.request)
+              .then(res => {
+                return caches.open(DYNAMIC_CACHE)
+                  .then(cache => {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                  })
+              });
           }
         })
     );
